8ca3f8d559aa1ab7d1bb69eb502d6c1eedc74890
Tiffany Lin <tlin@ICLPT00605.attlocal.net>
2016-07-27 21:18:50 -0700

    no message

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-<script src="node_modules/chart.js/Chart.min.js"></script>
-<script src="node_modules/angular-chart.js/dist/angular-chart.min.js"></script>
-
-aangular.module("app", ["chart.js"]).controller("DoughnutCtrl", function ($scope) {
-  $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  $scope.data = [300, 500, 100];
-});
-
-(function (ChartJsProvider) {
-  ChartJsProvider.setOptions({ colors : [ '#803690', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'] });
-});
+<script src="Chart.js"></script>
+<script>
+    var myChart = new Chart({...})
+</script>
