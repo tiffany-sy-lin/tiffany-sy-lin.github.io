ad17c9fed228f32055d9717106d5f624edf86f45
Tiffany Lin <tlin@ICLPT00605.attlocal.net>
2016-07-27 21:53:24 -0700

    no message

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-<script src="Chart.js"></script>
-<script>
-    var myChart = new Chart({...})
-</script>
+const CHART = document.getElementById("myChart");
+console.log(CHART);
+let myChart = new Chart(CHART, {
+  type: 'doughnut',
+  data: {
+    labels: [
+        "Red",
+        "Blue",
+        "Yellow"
+    ],
+    datasets: [
+        {
+            data: [300, 50, 100],
+            backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ]
+        }]
+  },
+  options: options
+})
