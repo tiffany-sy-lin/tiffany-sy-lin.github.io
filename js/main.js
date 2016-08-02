9af8f0c79fe3cc95b59fd4ddcdb85216023a3650
Tiffany Lin <tlin@ICLPT00605.local>
2016-08-02 01:22:43 -0700

    minor spacing edits and tweaks

--- js/main.js
+++ js/main.js
@@ -6,24 +6,24 @@ movieSalesApp.controller('theController', function ($scope) {
 
   //Define objects in controller
   $scope.graphLineSeries = ["Movie 1", "Movie 2"];
+  $scope.graphLineColors = ["#dcdcdc", "#26b192"];
   $scope.graphLineLabels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.graphLineDataPoints = [
     [65, 59, 40, 51, 36, 25, 40],
     [49, 49, 60, 39, 56, 38, 30]
   ];
-  $scope.graphLineColors = ["#dcdcdc", "#26b192"];
-  $scope.movies = {
+  $scope.movieInfo = {
     movie1: {
-        movieTitle: 'Movie 1',
-        movieTicketPurchases: 2346,
-        movieReleaseDate: '15 Jul 2015',
-        percentOfTotal: 35
+      movieTitle: 'Movie 1',
+      movieTicketPurchases: 2346,
+      movieReleaseDate: '15 July 2015',
+      percentOfTotal: 35
     },
     movie2: {
-        movieTitle: 'Movie 2',
-        movieTicketPurchases: 4422,
-        movieReleaseDate: '05 August 2015',
-        percentOfTotal: 65
+      movieTitle: 'Movie 2',
+      movieTicketPurchases: 4422,
+      movieReleaseDate: '01 August 2015',
+      percentOfTotal: 65
     }
   }
 });
